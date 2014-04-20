736c9f0d3ab187110d5675a7478f91d91a5b96c6
Yuyu <yuwendai.d@gmail.com>
2014-04-19 22:56:20 -0400

    Test For suannai

--- src/test/Test.java
+++ src/test/Test.java
@@ -18,13 +18,20 @@ public class Test {
 	
 	public static void main(String[] args) throws Exception{
 
-		Location loc = new Location("city1", "zip1", "MA", "USA");
+	/*	Location loc = new Location("city1", "zip1", "MA", "USA");
 		
 		Restaurant rest = new Restaurant(null, 3, 100, "businessid");
 		
 		Service service = new Service();
 		User user = service.getUserProfile("liyang920407@hotmail.com");
 		System.out.println(user.getFavoriteRestaurants().size());
+		*/
+		RestaurantDao rd = new RestaurantDao();
+		Restaurant rs = new Restaurant();
+		rs = rd.findRestaurantByBusinessId("RuCvHXtZKfkujxIWYpu1Gg");
+		System.out.println(rs.getId());
+		System.out.println(rs.getRating());
+		
 		
 	}
 }
